a4b10bd4c1662d9e7c3fdbb7f9248572aa820ed6
Zahid Dar <zdar@mail.ccsf.edu>
2019-07-18 23:20:13 -0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 const app = express();
-
+const port = process.env.PORT || 3000
 //Templating Engine
 app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
@@ -100,6 +100,6 @@ app.get('*', (req, res, next) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
+app.listen(port, () => {
+    console.log('Server started on port' + port)
 })
